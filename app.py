378ec3a0496c75748c2fcbc43f6d8d719d451284
SAMEER SHARMA <sameersharma@SAMEERs-MacBook-Air.local>
2026-09-25 14:52:39 +0530

    Add Streamlit user interface

--- app.py
+++ app.py
@@ -1,2 +1,8 @@
-print("Message Processor Application")
-print("Version 2")
+import streamlit as st
+
+st.title("Message Processor")
+
+message = st.text_input("Enter your message")
+ if st.button("Process Message"):
+     st.write("You entered:")
+st.write(message)
